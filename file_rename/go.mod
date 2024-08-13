0288100195a9d48a87c7e3909021cfd0cec681bf
Desislava Stefaanova <dst.stefanova@gmail.com>
2024-08-13 11:06:01 +0300

    Read excel file

--- file_rename/go.mod
+++ file_rename/go.mod
@@ -2,8 +2,26 @@ module takecontrolsoft.eu/file_rename
 
 go 1.22.4
 
+require github.com/xuri/excelize/v2 v2.8.1
+
 require (
+	github.com/go-errors/errors v1.5.1 // indirect
+	github.com/takecontrolsoft/go_multi_log/logger/levels v0.0.0-20240308174933-19cd21ec035b // indirect
+	github.com/takecontrolsoft/go_multi_log/logger/loggers v0.0.0-20240308174933-19cd21ec035b // indirect
+	github.com/timandy/routine v1.1.3 // indirect
+)
+
+require (
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
+	github.com/takecontrolsoft/go_multi_log/logger v0.0.0-20240308174933-19cd21ec035b
+	github.com/xuri/efp v0.0.0-20231025114914-d1ff6096ae53 // indirect
+	github.com/xuri/nfp v0.0.0-20230919160717-d98342af3f05 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/mod v0.20.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 	golang.org/x/tools v0.24.0 // indirect
 )
--- file_rename/main.go
+++ file_rename/main.go
@@ -1,7 +1,100 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/takecontrolsoft/go_multi_log/logger"
+	"github.com/takecontrolsoft/go_multi_log/logger/levels"
+	"github.com/takecontrolsoft/go_multi_log/logger/loggers"
+	"github.com/xuri/excelize/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fl, shouldReturn := RegisterLogger()
+	if shouldReturn {
+		return
+	}
+	defer func() {
+		fl.Stop()
+	}()
+
+	m, shouldReturn := ReadExcel("Clients.xlsx", "Clients", "ЕИК", "ИМЕ")
+	if shouldReturn {
+		return
+	}
+	subitems, err := os.ReadDir(".")
+	if err != nil {
+		logger.Error("Getting files from current folder failed")
+		return
+	}
+	for _, subitem := range subitems {
+		if !subitem.IsDir() {
+			logger.InfoF("File: %s", subitem.Name())
+			logger.Debug(m[""])
+		}
+	}
+}
+
+func ReadExcel(excelName string, sheetName string, eik string, name string) (map[string]string, bool) {
+	var m = make(map[string]string)
+	fe, err := excelize.OpenFile(excelName)
+	if err != nil {
+		logger.ErrorF("Open file %s failed, Error: %v", excelName, err)
+		return nil, true
+	}
+	defer func() {
+
+		if err := fe.Close(); err != nil {
+			logger.ErrorF("Closing file %s failed, Error: %v", excelName, err)
+		}
+	}()
+
+	rows, err := fe.GetRows(sheetName)
+	if err != nil {
+		logger.ErrorF("Getting rows from %s failed, Error: %v", sheetName, err)
+		return nil, true
+	}
+	var eikIndex, nameIndex int = 0, 0
+
+	for c, colCell := range rows[0] {
+		logger.InfoF("Row headers: %v", colCell)
+		if colCell == eik {
+			eikIndex = c
+		} else if colCell == name {
+			nameIndex = c
+		}
+	}
+	logger.InfoF("EIK index: %v, NAME index: %v", eikIndex, nameIndex)
+
+	for r, row := range rows {
+		if r > 0 {
+			logger.InfoF("Row index: %v", r)
+			eikValue := row[eikIndex]
+			nameValue := row[nameIndex]
+
+			m[eikValue] = nameValue
+			logger.InfoF("EIK: %s, NAME: %s", eikValue, nameValue)
+		}
+	}
+	return m, false
+}
+
+func RegisterLogger() (*loggers.FileLogger, bool) {
+	fileOptions := loggers.FileOptions{
+		Directory:     "./logs",
+		FilePrefix:    "file_rename",
+		FileExtension: ".log",
+	}
+	err := os.MkdirAll(fileOptions.Directory, os.ModePerm)
+	if err != nil {
+		return nil, false
+	}
+	fl := loggers.NewFileLogger(levels.All, "", fileOptions)
+
+	err = logger.RegisterLogger("file_rename", fl)
+	if err != nil {
+		logger.ErrorF("Creating log file failed %v", err)
+		return nil, true
+	}
+	return fl, false
 }
